a97e2217d21856aef296a8ce0c667e24790df9fd
WildfootW <wild.foot.yee.tzwu@gmail.com>
2019-04-22 15:36:32 +0800

    test

---
+++ test-makefile/my_util_io.cpp
@@ -0,0 +1,17 @@
+/*
+ * Version 
+ * Author: WildfootW
+ * GitHub: github.com/WildfootW
+ * Copyright (C) 2019 WildfootW All rights reserved.
+ *
+ */
+
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
+
