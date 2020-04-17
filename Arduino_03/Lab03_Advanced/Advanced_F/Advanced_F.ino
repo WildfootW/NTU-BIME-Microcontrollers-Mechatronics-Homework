0a045ccddc9614f720abef76343d7e37f26b8498
WildfootW <wildfootw@wildfoo.tw>
2020-04-17 21:50:39 +0800

    update

---
+++ Arduino_03/Lab03_Advanced/Advanced_F/Advanced_F.ino
@@ -0,0 +1,56 @@
+void setup() {
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(5, HIGH);// go forward 100
+  digitalWrite(6, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+  delay(4800);
+  digitalWrite(5, HIGH);// turn right 90 angle
+  digitalWrite(6, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, HIGH);
+  delay(350);
+  digitalWrite(5, HIGH);// go forward 50
+  digitalWrite(6, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+  delay(2400);
+  digitalWrite(5, LOW);// go backward 50
+  digitalWrite(6, HIGH);
+  digitalWrite(10, LOW);
+  digitalWrite(11, HIGH);
+  delay(2400);
+  digitalWrite(5, HIGH);// turn right 90 angle
+  digitalWrite(6, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, HIGH);
+  delay(350);
+  digitalWrite(5, HIGH);// go forward 50
+  digitalWrite(6, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+  delay(2400);
+  digitalWrite(5, LOW);// turn left 90 angle
+  digitalWrite(6, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+  delay(350);
+  digitalWrite(5, HIGH);// go forward 30
+  digitalWrite(6, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+  delay(1800);
+  digitalWrite(5, LOW);// stop
+  digitalWrite(6, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  delay(3000);
+
+}
